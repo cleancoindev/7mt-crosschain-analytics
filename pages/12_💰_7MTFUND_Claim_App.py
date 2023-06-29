cb6764d03b184c2ec2fa2397e362e1ade3e6b3b7
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-06-29 10:25:46 -1000

    Create 12_💰_7MTFUND_Claim_App.py

---
+++ pages/12_💰_7MTFUND_Claim_App.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="7MT-Fund Matic Claim",
+    page_icon="💰",
+)
+
+st.write("# Welcome to 7MT Fund Claim App Information Hub Page! 💰")
+
+st.markdown(
+    """
+    ### Want to learn more?
+    - Check out https://7-mt-fund-tokenclaim.vercel.app/
+<iframe
+    src="https://bafybeibl5uhjqhk74qhrtaw76hruylxcexcl2z36evkscw2jul254cuwje.ipfs-public.thirdwebcdn.com/erc20.html?contract=0x793Af35DEec10Bf981a81C591615f4af2901F7DE&chain=%7B%22name%22%3A%22Polygon+Mainnet%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fpolygon.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22matic%22%2C%22chainId%22%3A137%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22polygon%22%7D&theme=dark&primaryColor=purple"
+    width="600px"
+    height="600px"
+    style="max-width:100%;"
+    frameborder="0"
+></iframe>
+"""
+)
